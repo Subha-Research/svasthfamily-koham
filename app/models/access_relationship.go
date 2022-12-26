b00b5d45f1bdd15171e4830f87940f4be23b4ac6
RoshanShaw001 <99811536+RoshanShaw001@users.noreply.github.com>
2022-12-26 11:04:20 +0530

    Update Access Relationship

--- app/models/access_relationship.go
+++ app/models/access_relationship.go
@@ -20,7 +20,7 @@ type AccessRelationshipModel struct {
 	Session    *mongo.Session
 }
 
-func (arm *AccessRelationshipModel) GetSFAccessRelationship(f_parent_user_id string, f_child_user_id string) (bson.M, error) {
+func (arm *AccessRelationshipModel) GetAccessRelationship(f_parent_user_id string, f_child_user_id string) (bson.M, error) {
 	var result bson.M
 	err := arm.Collection.FindOne(
 		context.TODO(),
@@ -38,16 +38,16 @@ func (arm *AccessRelationshipModel) GetSFAccessRelationship(f_parent_user_id str
 	return result, nil
 }
 
-func (arm *AccessRelationshipModel) InsertAllSFAccessRelationship(f_head_user_id string, rb validators.ACLPostBody) (bson.M, error) {
+func (arm *AccessRelationshipModel) InsertAllAccessRelationship(f_head_user_id string, rb validators.ACLPostBody) (bson.M, error) {
 	// Colllection variable is set via Dependency injection from app file
 	var access_list_docs []interface{}
-	access_list := rb.ChildMemberAccessList
+	access_list := rb.AccessList
 	for i := 0; i < len(access_list); i++ {
 		access_relation := &sf_schemas.AccessRelationshipSchema{
 			AccessRelationshipID: uuid.NewString(),
-			ChildFamilyUserID:    access_list[i]["child_member_id"].(string),
+			ChildFamilyUserID:    access_list[i].ChildMemberId,
 			ParentFamilyUserID:   rb.ParentMemberID,
-			AccessEnum:           access_list[i]["access_enums"].([]interface{}),
+			AccessEnum:           access_list[i].AccessEnums,
 			IsDelete:             false,
 			Audit: sf_schemas.AuditSchema{
 				CreatedAt: time.Now(),
@@ -59,6 +59,36 @@ func (arm *AccessRelationshipModel) InsertAllSFAccessRelationship(f_head_user_id
 		access_list_docs = append(access_list_docs, access_relation)
 	}
 
+	// Call insert many of mongo
+	if len(access_list_docs) > 0 {
+		opts := options.InsertMany().SetOrdered(false)
+		res, err := arm.Collection.InsertMany(context.TODO(), access_list_docs, opts)
+		if err != nil {
+			log.Println("Error in inserting access relation", err)
+			return nil, err
+		}
+		fmt.Printf("Inserted documents with IDs %v\n", res.InsertedIDs)
+	}
+	return nil, nil
+}
+
+func (arm *AccessRelationshipModel) UpdateAllAccessRelationship(f_head_user_id string, rb validators.ACLPutBody) (bson.M, error) {
+	// Colllection variable is set via Dependency injection from app file
+	var access_list_docs []interface{}
+	access_list := rb.Access
+
+	access_relation := &sf_schemas.AccessRelationshipSchema{
+		AccessRelationshipID: uuid.NewString(),
+		ChildFamilyUserID:    access_list.ChildMemberId,
+		ParentFamilyUserID:   rb.ParentMemberID,
+		AccessEnum:           access_list.AccessEnums,
+		IsDelete:             false,
+		Audit: sf_schemas.AuditSchema{
+			UpdatedAt: time.Now(),
+			UpdatedBy: f_head_user_id,
+		},
+	}
+	access_list_docs = append(access_list_docs, access_relation)
 	// Call insert many of mongo
 	if len(access_list_docs) > 0 {
 		opts := options.InsertMany().SetOrdered(false)
